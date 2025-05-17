a48a49dec264028fe9288ce41b876f015f6ef214
SpreadSheets600 <sohammaity006@gmail.com>
2025-05-17 12:46:46 +0530

    feat: updated and fixed predict command

--- cogs/commands.py
+++ cogs/commands.py
@@ -1,8 +1,7 @@
 import discord
 from discord.ext import bridge, commands
-from utilities.predict import extract_pokemon_name
 from utilities.identify import predict_pokemon_from_url
-
+from utilities.predict import extract_pokemon_name, get_pokemon_sprite_name
 
 class CommandsCOG(commands.Cog):
     def __init__(self, bot):
@@ -12,21 +11,15 @@ class CommandsCOG(commands.Cog):
         name="predict",
         description="Predict Pokémon From Image URL Or Attachment",
     )
-    async def predict(
-        self,
-        ctx,
-        image_url: str = None,
-        image: discord.Attachment = None,
-    ):
+    async def predict(self, ctx, image_url: str = None):
         await ctx.defer()
 
+        image = ctx.message.attachments[0] if ctx.message.attachments else None
+
         if image_url:
-            name = await extract_pokemon_name(image_url)
+            name = extract_pokemon_name(image_url)
             if not name:
                 predictions = await predict_pokemon_from_url(image_url)
-                if not predictions:
-                    await ctx.reply("Could Not Identify The Pokémon!", ephemeral=True)
-                    return
                 top_prediction = max(predictions, key=lambda x: x[1])
                 name, score = top_prediction
             else:
@@ -38,27 +31,29 @@ class CommandsCOG(commands.Cog):
                 return
 
             predictions = await predict_pokemon_from_url(image.url)
-            if not predictions:
-                await ctx.reply("Could Not Identify The Pokémon!", ephemeral=True)
-                return
             top_prediction = max(predictions, key=lambda x: x[1])
             name, score = top_prediction
 
         else:
             await ctx.reply(
-                "Please Provide An Iage URL Or Attach An Image!", ephemeral=True
+                "Please Provide An Image URL Or Attach An Image!", ephemeral=True
             )
             return
 
+        sprite = get_pokemon_sprite_name(name)
+
         embed = discord.Embed(
             title="🔮 Pokémon Prediction",
-            description=f"### Predicted Pokémon : {name}",
+            description=f"##  Pokémon : {name.capitalize()}",
             color=0xFFCA7B,
         )
+
+        if sprite:
+            embed.set_thumbnail(url=sprite)
+
         embed.set_footer(text="AI Prediction May Not Be Accurate")
 
         await ctx.reply(embed=embed)
-
 
 def setup(bot):
     bot.add_cog(CommandsCOG(bot))
