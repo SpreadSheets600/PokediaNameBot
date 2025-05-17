bcee78e6c50a642b7027c7433b1d0e8cbd37d426
SpreadSheets600 <sohammaity006@gmail.com>
2025-05-17 12:47:02 +0530

    feat: updated prediction system and improved messages

--- cogs/predictor.py
+++ cogs/predictor.py
@@ -2,7 +2,9 @@ import re
 import discord
 from discord.ext import commands
 from utilities.solver import solve_hint
-from utilities.predict import extract_pokemon_name
+from utilities.identify import predict_pokemon_from_url
+from utilities.collector import get_collectors_for_pokemon
+from utilities.predict import extract_pokemon_name, get_pokemon_sprite_url
 
 
 class PredictorCOG(commands.Cog):
@@ -11,12 +13,19 @@ class PredictorCOG(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
-        print(message)
-
-        if message.author.id == 727012870683885578:
+        if message.author.id == 1338124574583427092:
             print("Message Received!")
 
-            if "Here's your hint".lower() in message.content.lower():
+            if "That's not the correct Pokémon! Guess the Pokémon correctly!".lower() in message.content.lower():
+                embed = discord.Embed(
+                    title = "Wrong Pokémon",
+                    description="## Pokémon Is Wrong! Please Use `p!h` To Get Hints.",
+                    color=0xFFCA7B,
+                )
+
+                await message.reply(embed=embed, mention_author=False)
+
+            elif "Here's your hint".lower() in message.content.lower():
                 match = re.search(
                     r"here's your hint:\s*`(.+?)`",
                     message.content.lower(),
@@ -26,35 +35,70 @@ class PredictorCOG(commands.Cog):
                 if match:
                     hint_pattern = match.group(1)
                     matches = solve_hint(hint_pattern)
+
                     if matches:
-                        response = f"🧩 Possible Pokémon: {', '.join(matches)}"
+                        embed = discord.Embed(
+                            title="🧩 Pokémon Hint Solve",
+                            description=f"## Possible Matches :\n {'\n'.join(matches)}",
+                            color=0xFFCA7B,
+                        )
 
-                    embed = discord.Embed(
-                        title="🧩 Pokémon Hint Solve",
-                        description=f"### Possible Matches :\n {'\n'.join(matches)}",
-                        color=0xffca7b,
-                    )
+                        await message.reply(embed=embed, mention_author=False)
 
-                    await message.reply(embed=embed, mention_author=False)
+                    else:
+                        embed = discord.Embed(
+                            title="🧩 Pokémon Hint Solve",
+                            description=f"### Unable To Solve Hint!",
+                            color=0xFFCA7B,
+                        )
+
+                        await message.reply(embed=embed, mention_author=False)
 
             elif message.embeds:
                 if (
-                    message.embeds[0].title
-                    and "wild" in message.embeds[0].title.lower()
-                ):
-                    print("[ + ] A Pokémon Spawned - Attempting To Predict")
+                        message.embeds
+                        and message.embeds[0].title
+                        and "wild" in message.embeds[0].title.lower()
+                    ):
+                        print("[ + ] A Pokémon Spawned - Attempting To Predict")
 
-                    image_url = message.embeds[0].image.url
+                        image_url = message.embeds[0].image.url
 
-                    predictions = await extract_pokemon_name(image_url)
+                        name = extract_pokemon_name(image_url)
+                        sprite = get_pokemon_sprite_url(image_url)
 
-                    embed = discord.Embed(
-                        title="🔮 Pokémon Prediction",
-                        description=f"### Predicted Pokémon : {predictions}",
-                        color=0xffca7b,
-                    )
+                        if not name:
+                            predictions = await predict_pokemon_from_url(image_url)
+                            top_prediction = max(predictions, key=lambda x: x[1])
+                            name, score = top_prediction
 
-                    await message.reply(embed=embed, mention_author=False)
+                        embed = discord.Embed(
+                            title="🔮 Pokémon Prediction",
+                            description=f"## Pokémon : {name.capitalize()}",
+                            color=0xFFCA7B,
+                        )
+                        embed.set_thumbnail(url=sprite)
+                        embed.set_footer(text="AI Prediction May Not Be Accurate")
+
+                        user_ids = get_collectors_for_pokemon(name)
+
+                        if user_ids:
+                            mentions = []
+                            for user_id in user_ids:
+                                try:
+                                    mentions.append(f"<@{user_id}>")
+                                except Exception as e:
+                                    print(f"Failed to fetch user {user_id}: {e}")
+
+                            mention_text = "### 🐾 This Pokémon Is On Your Collection List!\n" + ", ".join(mentions)
+                        else:
+                            mention_text = ""
+
+                        await message.reply(
+                            content=mention_text if mention_text else None,
+                            embed=embed,
+                            mention_author=False
+                        )
 
 
 def setup(bot):
--- utilities/predict.py
+++ utilities/predict.py
@@ -1,16 +1,38 @@
-import re
-
+import requests
 from urllib.parse import urlparse, unquote
 
 
-def extract_pokemon_name(url: str) -> str:
+def extract_pokemon_name(url: str):
     path = urlparse(url).path
     path = unquote(path)
 
     if "output/" in path and path.endswith(".png"):
         name_with_extension = path.split("output/")[-1]
         name = name_with_extension.split(".png")[0]
-        return name
-    
+        return name.split("-")[0]
+
     return None
 
+
+def get_pokemon_sprite_url(url: str):
+    path = urlparse(url).path
+    path = unquote(path)
+
+    if "output/" in path and path.endswith(".png"):
+        name_with_extension = path.split("output/")[-1]
+        sprite_link = f"https://raw.githubusercontent.com/pokedia/sprites/main/pokemon_images/{name_with_extension}"
+        return sprite_link
+
+    return None
+
+def get_pokemon_sprite_name(name: str):
+    sprite_link = f"https://raw.githubusercontent.com/pokedia/sprites/main/pokemon_images/{name}.png"
+
+    try:
+        response = requests.head(sprite_link, timeout=5)
+        if response.status_code == 200:
+            return sprite_link
+        else:
+            return None
+    except requests.RequestException:
+        return None
